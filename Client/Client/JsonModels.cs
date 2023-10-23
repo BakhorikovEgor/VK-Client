4e1417a8b0ab82540146b5d0f49b77d11df8057e
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-10-23 15:40:57 +0300

    feat: pass class work

---
+++ Client/Client/JsonModels.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Client;
+
+internal class GetFriendsResponse
+{
+    [JsonPropertyName("response")]
+    public ResponseInnerInfo FriendsIds { get; set; }
+    
+    public class ResponseInnerInfo
+    {
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("items")]
+        public List<int> Ids { get; set; }
+    } 
+}
+
+internal class GetUsersResponse
+{
+    [JsonPropertyName("response")]
+    public List<UserData> UsersData { get; set; }
+}
+
+
+internal class UserData
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    
+    [JsonPropertyName("first_name")]
+    public string FirstName { get; set; }
+    
+    [JsonPropertyName("last_name")]
+    public string LastName { get; set; }
+    
+    [JsonPropertyName("can_access_closed")]
+    public bool CanAccessClosed { get; set; }
+    
+    [JsonPropertyName("is_closed")]
+    public bool IsClosed { get; set; }
+}
---
+++ Client/Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace Client;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var clientId = "51777023";
+        var redirectUri = "https://oauth.vk.com/blank.html";
+        var authString =
+            $"https://oauth.vk.com/authorize?client_id={clientId}&display=page&redirect_uri={redirectUri}&response_type=token&v=5.131";
+        authString = authString.Replace("&", "^&");
+
+        Process.Start(new ProcessStartInfo("cmd", $"/c start {authString}") { CreateNoWindow = true });
+
+        var client = new HttpClient();
+        var accessToken = Console.ReadLine();
+        var friendsData = await GetFriendsAsync(client, accessToken);
+
+        foreach (var friend in friendsData)
+        {
+            Console.WriteLine($"{friend.LastName} {friend.FirstName}");
+        }
+    }
+
+
+    private static async Task<List<UserData>> GetFriendsAsync(HttpClient client, string? accessToken)
+    {
+        var rawResponse =
+            await client.GetAsync($"https://api.vk.com/method/friends.get?access_token={accessToken}&v=5.131");
+
+        rawResponse.EnsureSuccessStatusCode();
+
+        var json = await rawResponse.Content.ReadAsStreamAsync();
+        var response = await JsonSerializer.DeserializeAsync<GetFriendsResponse>(json);
+
+
+        return await GetUsersDataAsync(client, accessToken, response.FriendsIds.Ids);
+    }
+
+
+    private static async Task<List<UserData>> GetUsersDataAsync(HttpClient client, string token, IList<int> userIds)
+    {
+        var rawResponse =
+            await client.GetAsync(
+                $"https://api.vk.com/method/users.get?user_ids={string.Join(",", userIds)}&access_token={token}&v=5.154");
+
+        rawResponse.EnsureSuccessStatusCode();
+        
+        var json = await rawResponse.Content.ReadAsStreamAsync();
+        var response = await JsonSerializer.DeserializeAsync<GetUsersResponse>(json);
+
+        return response.UsersData;
+    }
+}
